991e646a341d7f187a6880b850fa8aef9fad205b
Rodrigo Gonzalez <rodrigo.gonzalez.zura@gmail.com>
2020-08-04 18:15:21 -0400

    feat: ready loading animation

--- src/components/views/Loading/Loading.js
+++ src/components/views/Loading/Loading.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, Modal, StyleSheet, View} from 'react-native';
+import {Animated, Easing, Modal, StyleSheet, View} from 'react-native';
 
 const styles = StyleSheet.create({
     modalBackground: {
@@ -28,22 +28,51 @@ const styles = StyleSheet.create({
 const logo = require('../../../assets/images/logo-main.png')
 
 
-const Loading = () => {
-    return (
-        <Modal
-            transparent={true}
-            animationType={'none'}
-            visible={true}
-            onRequestClose={() => {
-                console.log('close modal')
-            }}>
-            <View style={styles.modalBackground}>
-                <View style={styles.activityIndicatorWrapper}>
-                    <Image source={logo} style={styles.logo}/>
+class Loading extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {spinAnim: new Animated.Value(0)}
+    }
+
+    componentDidMount() {
+        const {spinAnim} = this.state;
+        setTimeout(
+            function () {
+                Animated.loop(Animated.timing(
+                    spinAnim,
+                    {
+                        toValue: 1,
+                        duration: 2000,
+                        easing: Easing.linear,
+                        useNativeDriver: true
+                    }
+                )).start();
+            },
+            400);
+    }
+
+    render() {
+        const spin = this.state.spinAnim.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+        });
+        return (
+            <Modal
+                transparent={true}
+                animationType={'none'}
+                visible={true}
+                onRequestClose={() => {
+                    console.log('close modal')
+                }}>
+                <View style={styles.modalBackground}>
+                    <View style={styles.activityIndicatorWrapper}>
+                        <Animated.Image source={logo} style={{...styles.logo, transform: [{rotate: spin}]}}/>
+                    </View>
                 </View>
-            </View>
-        </Modal>
-    );
+            </Modal>
+        );
+    }
 }
 
 export default Loading;
